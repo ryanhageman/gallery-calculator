7eef1f8261157085ee38d26b409742843df17ee6
Ryan Hageman <improv42@gmail.com>
2019-10-30 15:07:50 -0500

    Update webpack source

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ activate :livereload
 activate  :external_pipeline,
           name: :webpack,
           command: build? ? 'yarn run build' : 'yarn run start',
-          source: 'site.js',
+          source: 'source',
           latency: 1
 
 # Layouts
