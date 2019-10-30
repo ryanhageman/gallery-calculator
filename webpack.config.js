dce62dabf30508951f4f6ec857c07f197bfa3c71
Ryan Hageman <improv42@gmail.com>
2019-10-30 14:11:24 -0500

    Setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,10 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: __dirname + '/source/javascripts/'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
   }
 }
