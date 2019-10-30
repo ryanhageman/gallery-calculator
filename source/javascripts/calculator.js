b1cbacda920b644a793ccfdb2c69b33dc6e39565
Ryan Hageman <improv42@gmail.com>
2019-10-30 12:25:25 -0500

    Refactor javascript

--- source/javascripts/calculator.js
+++ source/javascripts/calculator.js
@@ -1,103 +1,113 @@
-let Prints = {
-  paper_constant: 5,
-  canvas_constant: 7
-}
-
-let Originals = {
-  jackson_square_constant: .385,
-  canvas_constant: 1.16
-}
-
-let length, width, type, constant, price
-
-let setLength = () => {
-  length = Number(document.querySelector('.js-calculator__length').value)
-}
-
-let setWidth = () => {
-  width = Number(document.querySelector('.js-calculator__width').value)
-}
-
-let setType = () => {
-  document.querySelectorAll('.js-calculator__type').forEach((button) => {
-    if (button.checked) { type = button.value }
-  })
-}
-
-let calculatePricePerSquareInch = (length, width, price_constant) => {
-  return (length * width) * price_constant
-}
-
-let calculatePricePerLinearInch = (length, width, price_constant) => {
-  return (length + width) * price_constant
-}
-
-let roundPrice = (price) => {
-  let whole_price = Math.round(price)
-  let last_digit = whole_price.toString().slice(-1)
-  switch(last_digit) {
-  case '1':
-    last_digit = '0'
-    break
-  case '2':
-    last_digit = '0'
-    break
-  case '3':
-    last_digit = '5'
-    break
-  case '4':
-    last_digit = '5'
-    break
-  case '5':
-    last_digit = '5'
-    break
-  case '6':
-    last_digit = '5'
-    break
-  case '7':
-    last_digit = '5'
-    break
-  case '8':
-    last_digit = '9'
-    break
-  case '9':
-    last_digit = '9'
-    break
-  default:
-    last_digit = '0'
-  }
-  let array_price = whole_price.toString().split('')
-  array_price.pop()
-  array_price.push(last_digit)
-  let string_price = array_price.join('')
-  let number_price = Number(string_price)
-
-  if (last_digit === '9') {
-    number_price = Math.round(number_price / 10) * 10
+class Calculator {
+  constructor() {
+    this.Prints = {
+      paper: 5,
+      canvas: 7
+    }
+    this.Originals = {
+      jackson_square: .385,
+      canvas: 1.16
+    }
+    this.length = ''
+    this.width = ''
+    this.type = ''
+    this.price_constant
+    this.price = ''
   }
 
-  return number_price
-}
-
-let getPrice = () => {
-  if (type === 'original-canvas') {
-    constant = Originals.canvas_constant
-    price = calculatePricePerSquareInch(length, width, constant)
-  } else if (type === 'original-js') {
-    constant = Originals.jackson_square_constant
-    price = calculatePricePerSquareInch(length, width, constant)
-  } else if (type === 'print-paper') {
-    constant = Prints.paper_constant
-    price = calculatePricePerLinearInch(length, width, constant)
-  } else if (type === 'print-canvas') {
-    constant = Prints.canvas_constant
-    price = calculatePricePerLinearInch(length, width, constant)
-  } else {
-    price = '??'
+  setLength(length) {
+    this.length = Number(length)
   }
 
-  roundPrice(price)
-  document.querySelector('.js-calculator__price').innerHTML = `$${roundPrice(price)}`
+  setWidth(width) {
+    this.width = Number(width)
+  }
+
+  setType(artwork_type) {
+    this.type = artwork_type
+    this._setPriceConstant(artwork_type)
+  }
+
+  getPrice() {
+    switch (this.type) {
+    case 'original-canvas':
+    case 'original-js':
+      return this._roundPrice(this._calculatePricePerSquareInch())
+    case 'print-paper':
+    case 'print-canvas':
+      return this._roundPrice(this._calculatePricePerLinearInch())
+    default:
+      return this._roundPrice('')
+    }
+  }
+
+  _setPriceConstant(artwork_type) {
+    switch (artwork_type) {
+    case 'original-canvas':
+      this.price_constant = this.Originals.canvas
+      break
+    case 'original-js':
+      this.price_constant = this.Originals.jackson_square
+      break
+    case 'print-paper':
+      this.price_constant = this.Prints.paper
+      break
+    case 'print-canvas':
+      this.price_constant = this.Prints.canvas
+      break
+    default:
+      this.price_constant = 0
+    }
+  }
+
+  _roundPrice(price) {
+    if (price === '') return '??'
+
+    let new_last_digit = this._lastDigitRounded(price)
+    let array_price = Math.round(price).toString().split('')
+    array_price.splice(array_price.length - 1, 1, new_last_digit)
+    let new_price = Number(array_price.join(''))
+
+    if (new_last_digit === '9') new_price = Math.round(new_price / 10) * 10
+
+    return new_price
+  }
+
+  _lastDigitRounded(number) {
+    switch (Math.round(number).toString().slice(-1)) {
+    case '1':
+    case '2':
+      return '0'
+
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+      return '5'
+
+    case '8':
+    case '9':
+      return '9'
+
+    default:
+      return '0'
+    }
+  }
+
+  _calculatePricePerSquareInch() {
+    return (this.length * this.width) * this.price_constant
+  }
+
+  _calculatePricePerLinearInch() {
+    return (this.length + this.width) * this.price_constant
+  }
+}
+
+let clear = () => {
+  document.querySelector('.js-calculator__length').value = ''
+  document.querySelector('.js-calculator__width').value = ''
+  clearRadioButtons()
 }
 
 let clearRadioButtons = () => {
@@ -106,16 +116,40 @@ let clearRadioButtons = () => {
   })
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector('.js-calculator__print-paper-constant').innerHTML = `Paper: $${Prints.paper_constant} / linear inch`
-  document.querySelector('.js-calculator__print-canvas-constant').innerHTML = `Canvas: $${Prints.canvas_constant} / linear inch`
-  document.querySelector('.js-calculator__original-jackson-square-constant').innerHTML = `JS: $${Originals.jackson_square_constant} / square inch`
-  document.querySelector('.js-calculator__original-canvas-constant').innerHTML = `Canvas: $${Originals.canvas_constant} / square inch`
-  document.querySelector('.js-calculator__length').value = ''
-  document.querySelector('.js-calculator__width').value = ''
-  clearRadioButtons()
+let showPaperPrintPrice = () => {
+  document.querySelector('.js-calculator__print-paper').innerHTML = `Paper: $${calculator.Prints.paper} / linear inch`
+}
+
+let showCanvasPrintPrice = () => {
+  document.querySelector('.js-calculator__print-canvas').innerHTML = `Canvas: $${calculator.Prints.canvas} / linear inch`
+}
+
+let showJacksonSquareCanvasPrice = () => {
+  document.querySelector('.js-calculator__original-jackson-square').innerHTML = `JS: $${calculator.Originals.jackson_square} / square inch`
+}
+
+let showCanvasOriginalPrice = () => {
+  document.querySelector('.js-calculator__original-canvas').innerHTML = `Canvas: $${calculator.Originals.canvas} / square inch`
+}
+
+let activateCalculatorButton = () => {
   document.querySelector('.js-calculator__get-price-button').addEventListener('click', (event) => {
     event.preventDefault()
-    getPrice()
+    document.querySelector('.js-calculator__price').innerHTML = `$${calculator.getPrice()}`
   })
+}
+
+let showPrices = () => {
+  showPaperPrintPrice()
+  showCanvasPrintPrice()
+  showJacksonSquareCanvasPrice()
+  showCanvasOriginalPrice()
+}
+
+let calculator = new Calculator
+
+document.addEventListener('DOMContentLoaded', function () {
+  clear()
+  showPrices()
+  activateCalculatorButton()
 });
