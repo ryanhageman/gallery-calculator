46a2a1fc9b3f6f1684f1e76d35eac475f0ed4ba4
Ryan Hageman <improv42@gmail.com>
2019-11-01 14:57:06 -0500

    Setup Build Config
    Ready for GitHub Pages

--- config.rb
+++ config.rb
@@ -50,7 +50,9 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  set :build_dir, 'docs'
+  activate :relative_assets
+  activate :minify_css
+  activate :minify_javascript
+end
