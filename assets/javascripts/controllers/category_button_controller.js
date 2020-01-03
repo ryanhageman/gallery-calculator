a12c1889fa8bde8f64db5df7dca8d3b34689f9cc
Ryan Hageman <improv42@gmail.com>
2020-01-03 10:41:46 -0600

    Show and hide price list card based on the category button clicked

--- assets/javascripts/controllers/category_button_controller.js
+++ assets/javascripts/controllers/category_button_controller.js
@@ -5,7 +5,12 @@ export default class extends Controller {
     'categoryButton',
     'originalsDropdown',
     'printsDropdown',
-    'artworkMediumButton'
+    'artworkMediumButton',
+    'originalPaper',
+    'originalCanvas',
+    'originalJacksonSquare',
+    'printPaper',
+    'printCanvas'
   ]
 
   categorySelect() {
@@ -24,6 +29,73 @@ export default class extends Controller {
   }
 
   artworkMediumSelect() {
-    this.data.set('artworkMedium', event.target.value)
+    this.artworkMedium = event.target.value
+    this._closeAllPriceListGroups()
+
+    switch (this.artworkMedium) {
+      case 'original--paper':
+        this.originalPaperTarget.classList.add('is-open')
+        break
+      case 'original--canvas':
+        this.originalCanvasTarget.classList.add('is-open')
+        break
+      case 'original--jackson-square':
+        this.originalJacksonSquareTarget.classList.add('is-open')
+        break
+      case 'print--paper':
+        this.printPaperTarget.classList.add('is-open')
+        break
+      case 'print--canvas':
+        this.printCanvasTarget.classList.add('is-open')
+        break
+      default:
+        this._closeAllPriceListGroups()
+    }
+  }
+
+  get artworkMedium() {
+    return this.data.get('artworkMedium')
+  }
+
+  set artworkMedium(value) {
+    this.data.set('artworkMedium', value)
+  }
+
+  _closeAllPriceListGroups() {
+    this._closeOriginalPaperPriceList()
+    this._closeOriginalCanvasPriceList()
+    this._closeOriginalJacksonSquarePriceList()
+    this._closePrintPaperPriceList()
+    this._closePrintCanvasPriceList()
+  }
+
+  _closeOriginalPaperPriceList() {
+    if (this.hasOriginalPaperTarget) {
+      this.originalPaperTarget.classList.remove('is-open')
+    }
+  }
+
+  _closeOriginalCanvasPriceList() {
+    if (this.hasOriginalCanvasTarget) {
+      this.originalCanvasTarget.classList.remove('is-open')
+    }
+  }
+
+  _closeOriginalJacksonSquarePriceList() {
+    if (this.hasOriginalJacksonSquareTarget) {
+      this.originalJacksonSquareTarget.classList.remove('is-open')
+    }
+  }
+
+  _closePrintPaperPriceList() {
+    if (this.hasPrintPaperTarget) {
+      this.printPaperTarget.classList.remove('is-open')
+    }
+  }
+
+  _closePrintCanvasPriceList() {
+    if (this.hasPrintCanvasTarget) {
+      this.printCanvasTarget.classList.remove('is-open')
+    }
   }
 }
