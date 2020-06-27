895d0dd0448c8e05d4920d7ab37fcf5e04a99336
Ryan Hageman <improv42@gmail.com>
2020-06-27 15:58:09 -0500

    Add specs for price_helpers

--- spec/helpers/price_helpers_spec.rb
+++ spec/helpers/price_helpers_spec.rb
@@ -1,9 +1,34 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require_relative '../../helpers/price_helpers'
 
 RSpec.describe 'PriceHelpers' do
-  it 'checks that rspec is installed and working' do
-    expect(true).to eq(true)
+  let(:item) { Class.new { include PriceHelpers }.new }
+
+  context '#price' do
+    it 'calculates a rounded price per linear inch' do
+      result = item.price('linear', 3, 3, 3)
+
+      expect(result).to eq('20')
+    end
+
+    it 'calculates a rounded price per square inch' do
+      result = item.price('square', 3, 3, 3)
+
+      expect(result).to eq('25')
+    end
+  end
+
+  it '#price_per_linear_inch calculates the rounded price' do
+    result = item.price_per_linear_inch(7, 7, 7)
+
+    expect(result).to eq('100')
+  end
+
+  it '#price_per_square_inch calculates the rounded price' do
+    result = item.price_per_square_inch(9, 9, 9)
+
+    expect(result).to eq('730')
   end
 end
