80df2dffa5507d2a2f90964e9250a16e080a108c
Ryan Hageman <ryan@rhageman.com>
2020-08-06 21:27:13 -0500

    Change webpack config to use new directories
    * Change '/assets' to '/webpack'
    * Change 'javascripts' to 'javascript'

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  entry: './assets/javascripts/app.js',
+  entry: './webpack/javascript/app.js',
   output: {
     filename: 'index.js',
-    path: __dirname + '/source/javascripts/'
+    path: __dirname + '/source/javascript/',
   },
   module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-    ]
-  }
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+  },
 }
