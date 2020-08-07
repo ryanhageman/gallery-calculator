9b51ecb767e564565c3e122f394e7040d6ca01a6
Ryan Hageman <ryan@rhageman.com>
2020-08-07 16:28:44 -0500

    Add tests for CalculatorController#categorySelect

---
+++ cypress/integration/controllers/calculator_controller/categorySelect.js
@@ -0,0 +1,22 @@
+describe('Calculator Controller', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  describe('#categorySelect', () => {
+    it('shows options for originals', () => {
+      cy.findByTestId('original-button').click()
+      cy.findByTestId('originals-dropdown').should(
+        'not.have.class',
+        'is-hidden'
+      )
+      cy.findByTestId('prints-dropdown').should('have.class', 'is-hidden')
+    })
+
+    it('shows options for prints', () => {
+      cy.findByTestId('print-button').click()
+      cy.findByTestId('prints-dropdown').should('not.have.class', 'is-hidden')
+      cy.findByTestId('originals-dropdown').should('have.class', 'is-hidden')
+    })
+  })
+})
