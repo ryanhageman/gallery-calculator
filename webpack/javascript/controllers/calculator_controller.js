ec1d93fbce8de5b72418a92798744ac35cfb57e0
Ryan Hageman <ryan@rhageman.com>
2020-08-09 14:00:19 -0500

    Change artworkMediumButton to artMediumButton

--- webpack/javascript/controllers/calculator_controller.js
+++ webpack/javascript/controllers/calculator_controller.js
@@ -10,7 +10,7 @@ export default class extends Controller {
     'sizeInWords',
     'artMediumHeading',
     'categoryButton',
-    'artworkMediumButton',
+    'artMediumButton',
     'getPriceButton',
     'originalsDropdown',
     'printsDropdown',
--- webpack/javascript/controllers/category_button_controller.js
+++ webpack/javascript/controllers/category_button_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
     'categoryButton',
     'originalsDropdown',
     'printsDropdown',
-    'artworkMediumButton',
+    'artMediumButton',
     'originalPaper',
     'originalCanvas',
     'originalJacksonSquare',
